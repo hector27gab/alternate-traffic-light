0d660416f3ccad8d788e853d197521ff2707afb6
hector27gab <147425846+hector27gab@users.noreply.github.com>
2023-12-06 02:10:52 +0000

    alternate-japan-trafficlight

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [ color, setColor] = useState();
+
+	const handleChangeBtn = (active) => {
+		setColor(active);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="tube"></div>
+				<div className="container" id="Light">
+					<div class="row">
+						<div class="col-12 col-lg-12">
+							<button 
+								className={`fa-solid fa-circle text-danger colorRed ${color == "red" ? "active" : ""}`} 
+								onClick={() => handleChangeBtn("red")}>
+							</button>
+							<button 
+								className={`fa-solid fa-circle text-warning colorYellow ${color == "yellow" ? "active" : ""}`} 
+								onClick={() => handleChangeBtn("yellow")}>
+							</button>
+							<button 
+								className={`fa-solid fa-circle text-primary colorBlue ${color == "blue" ? "active" : ""}`} 
+								onClick={() => handleChangeBtn("blue")}>							
+							</button>
+						</div>
+					</div>
+				</div>
+		
+		</>
 	);
 };
 
